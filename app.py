4159d03364c838df4c0a48e4ccf9547cbd3b8acb
norainthere <129417183+norainthere@users.noreply.github.com>
2023-05-04 13:53:01 -0400

    py

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ import matplotlib.pyplot as plt
 # Fetch OpenAI API key from environment variable
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
-# Set up OpenAI Client
-openai_client = openai.ApiClient()
-
 # Set up audio processing parameters
 sr = 44100 # Sample rate
 n_fft = 2048 # FFT size
@@ -18,14 +15,11 @@ hop_length = n_fft // 4 # Hop length for STFT
 # Set up Matplotlib figure for the equalization curve
 fig, ax = plt.subplots()
 
-# Suppress warning message when calling st.pyplot()
-st.set_option('deprecation.showPyplotGlobalUse', False)
-
 # App framework
 st.title("Equalization Curve Recommendation")
 
 # Upload an audio file
-audio_file = st.file_uploader("Upload your audio file", type=["mp3", "wav", "ogg"])
+audio_file = st.file_uploader("Upload an audio file", type=["mp3", "wav", "ogg"])
 
 # Get user input for additional text prompt
 additional_prompt = st.text_input("Additional prompt (optional)")
@@ -33,13 +27,13 @@ additional_prompt = st.text_input("Additional prompt (optional)")
 # Generate equalization curve based on the uploaded audio file and additional prompt
 if audio_file is not None:
     # Load the audio file and extract its spectral content
-    y, _ = librosa.load(audio_file, sr=sr, mono=True)
+    y, _ = librosa.load(audio_file, sr=sr)
     D = librosa.stft(y, n_fft=n_fft, hop_length=hop_length, center=True)
     S = librosa.amplitude_to_db(abs(D), ref=1.0)
 
     # Use the OpenAI API to generate a text prompt for the equalization curve
     prompt = f"Generate an equalization curve for the uploaded audio file. {additional_prompt}"
-    response = openai_client.Completion.create(
+    response = openai.Completion.create(
         engine="davinci",
         prompt=prompt,
         max_tokens=2000
