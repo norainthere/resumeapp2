ef4445f570fb296c8f49963bbf70d3d21e5e18dc
norainthere <129417183+norainthere@users.noreply.github.com>
2023-05-16 17:57:10 -0400

    app.py

--- app.py
+++ app.py
@@ -1,83 +1,64 @@
+from typing import Tuple
+
+# App to generate equalization curves based on audio files and user input
+
 import streamlit as st
-import openai
 import librosa
 import matplotlib.pyplot as plt
-import os
+import numpy as np
 
-# Set up OpenAI Client
-openai.api_key = st.secrets["openai"]["openai_api_key"]
-openai_model = 'davinci'
-
-# Set up OpenAI Playground Trained Model
-playground_model = "ezra"
-
-# Set up audio processing parameters
-sr = 44100  # Sample rate
-n_fft = 2048  # FFT size
-hop_length = n_fft // 4  # Hop length for STFT
+# Constants 
+sr = 22050  
+n_fft = 2048 
+hop_length = 512
 
 # Set up Matplotlib figure for the equalization curve
 fig, ax = plt.subplots()
 
 # App framework
-st.title("Ezra🎚️🏳️‍⚧️")
+st.title("Ezra🎚️🏳️‍⚧️")  
 
 # Get user input for additional text prompt
-additional_prompt = st.text_input("Describe the qualities of your desired EQ")
+additional_prompt = st.text_input("Describe the qualities of your desired EQ")  
 
 # Get user input for specific quality and issue
 specific_quality = st.text_input("Specify the specific quality you want to enhance")
 specific_issue = st.text_input("Specify the specific issue you want to reduce")
 
 # Upload an audio file
-audio_file = st.file_uploader("Upload your audio file", type=["mp3", "wav", "ogg"])
+audio_file = st.file_uploader("Upload your audio file", type=["mp3", "wav", "ogg"])  
+
+# Function to extract audio features
+def extract_audio_features(audio_file: str) -> Tuple[np.ndarray, int]:
+    """Loads an audio file and returns its spectral features and sampling rate"""
+    try: 
+        y, sr = librosa.load(audio_file, sr=sr) 
+        D = librosa.stft(y, n_fft=n_fft, hop_length=hop_length, center=True)
+    except FileNotFoundError as err:
+        st.error("Audio file not found")
+    except Exception as err:
+        st.error("Error processing audio file")
+    return D, sr  
+
+# Generate equalization curve 
+def generate_eq_curve(D: np.ndarray, additional_prompt: str, 
+                      specific_quality: str, specific_issue: str) -> np.ndarray:
+    ...
+
+# Plot the equalization curve
+def plot_eq_curve(curve: np.ndarray, sr: int, ax: plt.Axes) -> None:
+    ...
 
 # Generate equalization curve based on the uploaded audio file and additional prompt
-if audio_file is not None:
-    # Load the audio file and extract its spectral content
-    try:
-        y, _ = librosa.load(audio_file, sr=sr)
-        D = librosa.stft(y, n_fft=n_fft, hop_length=hop_length, center=True)
-        S = librosa.amplitude_to_db(abs(D), ref=1.0)
-    except:
-        st.error("Unable to load audio file. Please try again with a different file.")
-        st.stop()
+if audio_file is not None:  
+    # Extract audio features 
+    D, sr = extract_audio_features(audio_file)  
 
-    # Use the OpenAI API to generate a text prompt for the equalization curve
-    prompt = f"Generate an equalization curve for the uploaded audio file. The audio has {additional_prompt}. The desired equalization should enhance the {specific_quality} and reduce the {specific_issue}."
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt=prompt,
-        max_tokens=4000
-    )
+    # Generate EQ curve
+    curve = generate_eq_curve(D, additional_prompt, specific_quality, specific_issue)
 
-    # Add error handling for empty response
-    if not response.choices:
-        st.error("No equalization curve found. Please try again with a different prompt.")
-        st.stop()
+    # Plot the equalization curve
+    plot_eq_curve(curve, sr, ax)  
 
-    eq_text = response.choices[0].text
-
-    # Validate and parse the equalization curve from the generated text
-    eq_curve = []
-    try:
-        eq_curve = list(map(float, eq_text.strip().split()))
-    except ValueError:
-        st.error("Invalid equalization curve format. Please try again with a different prompt.")
-        st.stop()
-
-    # Check if the parsed equalization curve is empty
-    if not eq_curve:
-        st.error("Equalization curve not found. Please try again with a different prompt.")
-        st.stop()
-
-    # Plot the equalization curve on the Matplotlib figure
-    ax.plot(eq_curve)
-
-    # Set the labels and title for the Matplotlib figure
-    ax.set_xlabel("Frequency (Hz)")
-    ax.set_ylabel("Gain (dB)")
-    ax.set_title("Recommended Equalization Curve")
-
-    # Display the Matplotlib figure in Streamlit
+    # Display the equalization curve
     st.pyplot(fig)
