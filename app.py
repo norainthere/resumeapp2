a3b9149f1c9e22bafb08f2009f62a612414e909c
norainthere <129417183+norainthere@users.noreply.github.com>
2023-05-16 19:06:40 -0400

    app.py

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import streamlit as st
 import librosa
 import matplotlib.pyplot as plt
 import numpy as np
+import torch
+import torchaudio
+from torch import nn
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
 
 # Constants
 sr = 22050  
@@ -12,6 +17,14 @@ hop_length = 512
 # Set up Matplotlib figure for the equalization curve
 fig, ax = plt.subplots()
 
+# Model 
+class EQModel(nn.Module):
+    ...
+
+model = EQModel()
+model.load_state_dict(torch.load('eq_model.pth'))
+model.eval()
+
 # App framework  
 st.title("Ezra🎚️🏳️‍⚧️")  
 
@@ -19,33 +32,35 @@ st.title("Ezra🎚️🏳️‍⚧️")
 additional_prompt = st.text_input("Describe the qualities of your desired EQ")  
 
 # Get user input for specific quality and issue
-specific_quality = st.text_input("Specify the specific quality you want to enhance")
-specific_issue = st.text_input("Specify the specific issue you want to reduce")
+specific_quality = st.selectbox("Select the quality to enhance", ["Bass", "Midrange", "Treble"])
+specific_issue = st.selectbox("Select the issue to reduce", ["Muddy", "Tinny", "Harsh", "Dull"])
 
 # Upload an audio file
 audio_file = st.file_uploader("Upload your audio file", type=["mp3", "wav", "ogg"])  
 
 # Function to extract audio features
-def extract_audio_features(audio_file: str) -> Tuple[np.ndarray, int]:
-    """Loads an audio file and returns its spectral features and sampling rate"""
-    try:
-        y, sr = librosa.load(audio_file, sr=sr)
-        D = librosa.stft(y, n_fft=n_fft, hop_length=hop_length, center=True)
-    except FileNotFoundError as err:
-        st.error("Audio file not found")
-    except Exception as err:
-        st.error("Error processing audio file")
-    return D, sr
+def extract_audio_features(audio_file: str) -> Tuple[torch.Tensor, int]:
+    audio, sr = torchaudio.load(audio_file)
+    D = torchaudio.transforms.MelSpectrogram(sr)(audio) 
+    D = torch.log(D + 1e-6) 
+    return D, sr  
 
 # Generate equalization curve 
-def generate_eq_curve(D: np.ndarray, additional_prompt: str, 
+def generate_eq_curve(D: torch.Tensor, additional_prompt: str, 
                       specific_quality: str, specific_issue: str) -> np.ndarray:
-    """Generates an equalization curve based on audio features and user prompts"""
-    ...
+    with torch.no_grad():
+        # Format inputs for the model
+        inputs = [D, additional_prompt, specific_quality, specific_issue]
+        inputs = [i.unsqueeze(0) for i in inputs]
+        
+        # Get model predictions
+        curve = model(*inputs)
+        curve = curve.squeeze(0).numpy()
+        
+    return curve
 
 # Plot the equalization curve
 def plot_eq_curve(curve: np.ndarray, sr: int, ax: plt.Axes) -> None:
-    """Plots the equalization curve on the Matplotlib figure"""
     ...
 
 # Generate equalization curve based on the uploaded audio file and additional prompt
@@ -61,3 +76,4 @@ if audio_file is not None:
 
     # Display the equalization curve
     st.pyplot(fig)
+
